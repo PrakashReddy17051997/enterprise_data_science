e243418f492a4c11cbff2cc292bf347df4e1ea08
Prakash Reddy Orakanti <prakash.reddyo1997@gmail.com>
2022-06-30 14:18:07 +0200

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='COVID-19 Infection_rate and Vaccination Rate over population size data Analysis',
+    author='Prakash Reddy Orakanti',
+    license='',
+)
